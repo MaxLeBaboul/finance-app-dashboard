57cc3a218ef2ab6d7fc5e2ee181a114727d7dc2f
Maxwell Lewis Baboula <m.baboula@commonfactor.tech>
2024-05-17 15:21:04 +0100

    Router and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
 
-import './App.css'
+import { Box, CssBaseline } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useMemo } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./scenes/navbar";
+import { themeSettings } from "./theme";
 
 function App() {
- // const [count, setCount] = useState(0)
+  const theme = useMemo(() => createTheme(themeSettings), []);
 
   return (
     <div className="app">
-
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<div>dashboard page</div>} />
+              <Route
+                path="/predictions"
+                element={<div>predictions page</div>}
+              />
+              <Route path="*" element={<div>Not Found</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
 export default App
