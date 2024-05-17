af7dda7e0f99edc8d5b865fa96bb499577f35398
Maxwell Lewis Baboula <m.baboula@commonfactor.tech>
2024-05-18 00:51:14 +0100

    Redux tolkit setup and configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import App from '@/App'
-import '@/index.css'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import App from "@/App";
+import "@/index.css";
+import { api } from "@/state/api";
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+export const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
+});
+
+setupListeners(store.dispatch);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>
+);
